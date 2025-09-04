aadbad69ba5e23e54aeb936fb77fb6f66065b1eb
Renan Nandi <renannandi@gmail.com>
2025-09-03 21:53:05 -0300

    Adiciona modificações para editar brindes - Conteúdo da aula 2 da semana 2 do módulo 3.

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,9 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" Component={EnvioForm}/>
-        <Route path="/envios" Component={EnvioList}/>
+        <Route path="/" Component={EnvioList}/>
+        <Route path="/envios/novo" Component={EnvioForm}/>
+        <Route path="/envios/editar/:id" Component={EnvioForm}/>
         <Route path='cadastroProduto' Component={CadastroProdutoForm}/>
         <Route path='produtos' Component={ListagemProdutos}/>
       </Routes>
--- src/pages/Envios/EnviosList/EnviosList.jsx
+++ src/pages/Envios/EnviosList/EnviosList.jsx
@@ -1,7 +1,102 @@
-import React from 'react'
+import {
+  Card,
+  CardContent,
+  Typography,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+} from "@mui/material";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from '@mui/icons-material/Edit';
+import { useNavigate } from "react-router-dom";
 
 export default function EnvioList() {
+  const [envios, setEnvios] = useState([]);
+
+  const navigate = useNavigate();
+
+  function buscarEnvios() {
+    axios
+      .get("http://localhost:3001/envios")
+      .then((res) => {
+        setEnvios(res.data);
+      })
+      .catch(() => {
+        console.log("Erro ao obter envios!");
+      });
+  }
+
+  useEffect(() => {
+    buscarEnvios();
+  }, []);
+
+  function deletarEnvio(id) {
+    if (confirm("Tem certeza que deseja deletar?")) {
+      axios
+        .delete(`http://localhost:3001/envios/${id}`)
+        .then(() => {
+          alert("Deletado com sucesso!");
+          buscarEnvios();
+        })
+        .catch(() => alert("Falha ao deletar!"));
+    }
+  }
+
+  function redirecionarParaEdicao (id) {
+    navigate(`/envios/editar/${id}`);
+  }
+
   return (
-    <div>EnvioList</div>
-  )
+    <div>
+      Tela de listagem
+      <Card>
+        <CardContent>
+          <Typography></Typography>
+
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Código</TableCell>
+                  <TableCell>Cliente</TableCell>
+                  <TableCell>Itens</TableCell>
+                  <TableCell>Valor</TableCell>
+                  <TableCell>Ações</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {envios.map((envio) => (
+                  <TableRow key={envio.id}>
+                    <TableCell>{envio.id}</TableCell>
+                    <TableCell>{envio.cliente_nome}</TableCell>
+                    <TableCell>{envio.produtos_clientes.length}</TableCell>
+                    <TableCell>
+                      {envio.valor_total?.toLocaleString("pt-BR", {
+                        minimumFractionDigits: 2,
+                        maximumFractionDigits: 2,
+                        style: "currency",
+                        currency: "BRL",
+                      })}
+                    </TableCell>
+                    <TableCell>
+                      <DeleteIcon
+                        style={{ color: "red" }}
+                        onClick={() => deletarEnvio(envio.id)}
+                      />
+                      <EditIcon style={{color: "#55F"}} onClick={()=>redirecionarParaEdicao(envio.id)}/>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
