550b2b74966e834ca131b952ea726aa1b77d7892
Renan Nandi <renannandi@gmail.com>
2025-09-04 16:01:08 -0300

    Implementa edição de produtos.

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Brindes
 
 Cadastro de brindes com React
+
+
+## Rotas
+
+"/" : Lista de envios
+"/envios/novo": Cadastrar novo envio
+"/envios/editar/:id": Editar envio existente
+
+'/produtos': ListagemProdutos
+'/produtos/novo': Cadastrar novo produto
+'/produtos/editar/:id': Editar produto existente
--- src/pages/CadastroProduto/CadastroProdutoForm.jsx
+++ src/pages/CadastroProduto/CadastroProdutoForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   TextField,
   Select,
@@ -17,6 +17,7 @@ import InputAdornment from "@mui/material/InputAdornment";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import AddBoxIcon from "@mui/icons-material/AddBox";
 import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function CadastroProdutoForm() {
   const [nome, setNome] = useState("");
@@ -24,6 +25,9 @@ export default function CadastroProdutoForm() {
   const [precoNumerico, setPrecoNumerico] = useState(0);
   const [descricao, setDescricao] = useState("");
   const [urlImagem, setUrlImagem] = useState("");
+
+  const params = useParams();
+  const navigate = useNavigate();
 
   const formatarPreco = (valor) => {
     if (!valor) return "";
@@ -56,83 +60,93 @@ export default function CadastroProdutoForm() {
       imagem: urlImagem,
     };
 
-    axios
-      .post("http://localhost:3001/produtos", produto)
-      .then((res) => {
-        console.log("Produto cadastrado com sucesso!", res);
-        setNome("");
-        setDescricao("");
-        setPreco("");
-        setUrlImagem("");
-        setPrecoNumerico(0)
-      })
-      .catch((err) => console.log("Erro ao cadastrar produto! ", err));
-
+    axios({
+        url: params.id ? `http://localhost:3001/produtos/${params.id}` : "http://localhost:3001/produtos", 
+        data: produto,
+        method: params.id ? 'PUT' : "POST"
+        })
+        .then(() => {
+          alert("Produto cadastrado com sucesso!");
+          navigate("/produtos");
+        })
+        .catch(() => alert("Erro ao cadastrar produto!"));
   }
+
+  useEffect(()=> {
+    if (params.id) {
+      axios.get(`http://localhost:3001/produtos/${params.id}`)
+      .then((res)=> {
+          setNome(res.data.nome);
+          setPreco(res.data.preco);
+          setPrecoNumerico(Number(res.data.preco));
+          setDescricao(res.data.descricao);
+          setUrlImagem(res.data.imagem);
+      })
+      .catch(()=>console.log("Erro ao obter produto!"))
+    }
+  },[])
 
   return (
     <>
-      <>
-        <Card variant="outlined">
-          <CardContent
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-              gap: "20px",
-            }}
-          >
-            <Typography as="h1">Cadastro de Produto</Typography>
+      <Card variant="outlined">
+        <CardContent
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "20px",
+          }}
+        >
+          <Typography as="h1">Cadastro de Produto</Typography>
 
-            <TextField
-              fullWidth
-              label="Nome do Produto"
-              onChange={(e) => setNome(e.target.value)}
-              value={nome}
+          <TextField
+            fullWidth
+            label="Nome do Produto"
+            onChange={(e) => setNome(e.target.value)}
+            value={nome}
+          />
+
+          <FormControl sx={{ m: 1 }} fullWidth>
+            <InputLabel htmlFor="outlined-adornment-preco">Preço</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-preco"
+              startAdornment={
+                <InputAdornment position="start">R$</InputAdornment>
+              }
+              label="Preço"
+              onChange={handlePriceChange}
+              value={preco}
             />
+          </FormControl>
 
-            <FormControl sx={{ m: 1 }} fullWidth>
-              <InputLabel htmlFor="outlined-adornment-preco">Preço</InputLabel>
-              <OutlinedInput
-                id="outlined-adornment-preco"
-                startAdornment={
-                  <InputAdornment position="start">R$</InputAdornment>
-                }
-                label="Preço"
-                onChange={handlePriceChange}
-                value={preco}
-              />
-            </FormControl>
+          <TextField
+            fullWidth
+            multiline
+            minRows={3}
+            label="Descrição"
+            onChange={(e) => setDescricao(e.target.value)}
+            value={descricao}
+          />
 
-            <TextField
-              fullWidth
-              multiline
-              minRows={3}
-              label="Descrição"
-              onChange={(e) => setDescricao(e.target.value)}
-              value={descricao}
-            />
+          <TextField
+            fullWidth
+            label="🔗 URL da Imagem"
+            onChange={(e) => setUrlImagem(e.target.value)}
+            value={urlImagem}
+          />
 
-            <TextField
-              fullWidth
-              label="🔗 URL da Imagem"
-              onChange={(e) => setUrlImagem(e.target.value)}
-              value={urlImagem}
-            />
-
-            <Box style={{ display: "flex", gap: "10px" }}>
-              <Button
-                variant="contained"
-                endIcon={<AddBoxIcon />}
-                onClick={cadastrarProduto}
-              >
-                Cadastrar
-              </Button>
-            </Box>
-          </CardContent>
-        </Card>
-      </>
+          <Box style={{ display: "flex", gap: "10px" }}>
+            <Button
+              variant="contained"
+              endIcon={<AddBoxIcon />}
+              onClick={cadastrarProduto}
+            >
+              Cadastrar
+            </Button>
+          </Box>
+        </CardContent>
+      </Card>
     </>
   );
 }
